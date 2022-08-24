69bcb2804ed5ea484969a033321e57ac726b7c86
Sahil4596 <66122476+Sahil4596@users.noreply.github.com>
2022-08-24 23:13:08 +0530

    Add files via upload

---
+++ sahil1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char myChar='A';
+  char myOtherChar=65;
+  cout << "Value of myChar is: "<< myChar<<endl;
+  cout << "Value of myOtherChar is: "<< myOtherChar<<endl;
+  return 0;
+}
---
+++ sahil3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  float myFloat;  // Creates a floating point variable
+  myFloat = 8.3555565656565656565656;  // Stores 8.3 in the new variable
+  cout<< "Value of myFloat is: "<< myFloat << endl;
+
+  
+  return 0;
+}
